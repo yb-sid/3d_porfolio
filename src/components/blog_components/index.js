8f8d1807f71910cf1092a61c0042cf0946d28635
Siddharth Kumar <ybsid@Siddharths-MacBook-Pro.local>
2023-11-03 00:34:12 +0530

    export blog components

---
+++ src/components/blog_components/index.js
@@ -0,0 +1,17 @@
+import BlogNavbar from './navbar/BlogNavbar'
+import Footer from './footer/Footer'
+import Featured from './featured/Featured';
+import CategoryList from './categoryList/CategoryList';
+import CardList from './cardList/CardList';
+import Menu from './menu/Menu'
+import Pagination from './pagination/Pagination';
+
+export {
+    BlogNavbar,
+    Footer,
+    Featured,
+    CategoryList,
+    CardList,
+    Menu,
+    Pagination
+}
