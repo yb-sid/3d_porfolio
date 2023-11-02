22d3df03d4c5a866bb815f7895fea7b935f5b45c
Siddharth Kumar <ybsid@Siddharths-MacBook-Pro.local>
2023-11-03 00:25:25 +0530

    refactor app.jsx add blog styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,49 @@
 
-import { BrowserRouter } from "react-router-dom"
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Works, Tech, StarsCanvas } from "./components"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import {
+  About, Contact, Experience, Feedbacks,
+  Hero, Navbar, Works, Tech, StarsCanvas, Blogs
+} from "./components"
+
+import { ApolloProvider } from "@apollo/client"
+import client from "./apollo/apolloConfig"
+
+
+const Home = () => {
+  return (
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+
+      <div className="relative z-0">
+        <Tech />
+        <StarsCanvas />
+      </div>
+
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+
+  )
+}
 const App = () => {
 
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-
-        <div className="relative z-0">
-          <Tech />
-          <StarsCanvas />
-        </div>
-
-        <Works />
-        <Feedbacks />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
+      <ApolloProvider client={client}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+        </Routes>
+      </ApolloProvider>
     </BrowserRouter>
   )
 }
