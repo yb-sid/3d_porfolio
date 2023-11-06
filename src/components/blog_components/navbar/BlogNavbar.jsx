4a9953650b0dca981e741dfe7e3bc99347401b7a
Siddharth Kumar <ybsid@Siddharths-MacBook-Pro.local>
2023-11-06 18:23:37 +0530

    Blog Nav :: use cat slug

--- src/components/blog_components/navbar/BlogNavbar.jsx
+++ src/components/blog_components/navbar/BlogNavbar.jsx
@@ -3,12 +3,13 @@ import styles from './navbar.module.css'
 import { Link } from "react-router-dom";
 
 
-const BlogNavbar = () => {
+const BlogNavbar = ({ catPage }) => {
     return (
         <div className={styles.container}>
             <div className={styles.logo}>Way of the Sid</div>
             <div className={styles.links}>
                 <Link to="/" >Home</Link>
+                {catPage && <Link to="/blogs">Blog Home</Link>}
             </div>
         </div>
     )
