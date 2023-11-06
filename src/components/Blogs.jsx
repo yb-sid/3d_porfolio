9c565da236f4f5d95049670c3e44b3b65f83202b
Siddharth Kumar <ybsid@Siddharths-MacBook-Pro.local>
2023-11-06 18:38:48 +0530

    clean Blogs.jsx

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,22 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { StarsCanvas } from "./canvas";
 import { BlogNavbar, Featured, CategoryList, Footer, CardList, Menu } from "./blog_components"
 import styles from "./blogs.module.css"
 
 const BlogComponents = () => {
-
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-
   return (
     <div className={styles.container}>
       <Featured />
       <CategoryList />
       <div className={styles.content}>
-        <CardList page={currentPage} onPageChange={handlePageChange} />
+        <CardList categoryName="" />
         <Menu />
       </div>
     </div>
